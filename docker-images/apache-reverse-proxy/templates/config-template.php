a601f17e9148acd682f48a72bad5812950ff246e
Gaetan2907 <gaetan.daubresse@gmail.com>
2020-05-04 09:04:26 +0200

    First configuration of the load balancer, doesn't work yet

--- docker-images/apache-reverse-proxy/templates/config-template.php
+++ docker-images/apache-reverse-proxy/templates/config-template.php
@@ -1,15 +1,28 @@
 <?php 
-	$dynamic_app = getenv('DYNAMIC_APP'); 
-	$static_app = getenv('STATIC_APP'); 
+	$dynamic_app1 = getenv('DYNAMIC_APP1'); 
+	$dynamic_app2 = getenv('DYNAMIC_APP2');
+	$static_app1 = getenv('STATIC_APP1'); 
+	$static_app2 = getenv('STATIC_APP1'); 
 ?>
 <VirtualHost *:80> 
 	ServerName demo.res.ch
 	
-	ProxyPass '/api/students/' 'http://<?php print "$dynamic_app"?>/'
-	ProxyPassReverse '/api/students/' 'http://<?php print "$dynamic_app"?>/'
+	<Proxy "balancer://dynamic>
+		BalancerMember 'http://<?php print "$dynamic_app1"?>/' route=1
+		BalancerMember 'http://<?php print "$dynamic_app2"?>/' route=2
+	</Proxy> 
 	
-	ProxyPass '/' 'http://<?php print "$static_app"?>/' 
-	ProxyPassReverse '/' 'http://<?php print "$static_app"?>/' 
+	<Proxy "balancer://static> 
+		BalancerMember 'http://<?php print "$static_app1"?>/' route=1
+		BalancerMember 'http://<?php print "$static_app2"?>/' route=2
+	</Proxy> 
+	
+	ProxyPass '/api/students/' 'balancer://dynamic'
+	ProxyPassReverse '/api/students/' 'balancer://dynamic'
+	
+	
+	ProxyPass '/' 'balancer://static' 
+	ProxyPassReverse '/' 'balancer://static' 
 	
 </VirtualHost> 
 
