010571dc2030254b3b1d95893bc37f4cbfb6f0d5
Gaetan2907 <gaetan.daubresse@gmail.com>
2020-05-04 16:49:56 +0200

    Add script init.sh to build and run all the containers

---
+++ init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd docker-images/apache-php-image
+docker build -t res/apache_php . 
+
+cd ../apache-reverse-proxy 
+docker build -t res/apache_rp .
+
+cd ../express-image 
+docker build -t res/express_locations . 
+
+docker run -d --name express_dynamic1 res/express_locations
+docker run -d --name express_dynamic2 res/express_locations
+
+docker run -d --name apache_static1 res/apache_php
+docker run -d --name apache_static2 res/apache_php
+
+docker run -d -e STATIC_APP1=172.17.0.4:80 -e STATIC_APP2=172.17.0.5:80 -e DYNAMIC_APP1=172.17.0.2:3000 -e DYNAMIC_APP2=172.17.0.3:3000 --name apache_rp -p 8080:80 res/apache_rp
+
